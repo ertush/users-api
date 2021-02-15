e7e3ccb4a18fe7c491e324963e9fb8aea10d3943
ertush <erotush77@gmail.com>
2021-02-15 19:51:46 +0300

    modified cmd/api/main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -152,7 +152,7 @@ func main() {
 	handler.HandleFunc(reg3, createUsersHandler)
 
 	s := &http.Server{
-		Addr:           "",
+		Addr:           ":8080"
 		Handler:        handler,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
