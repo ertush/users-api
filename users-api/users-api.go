834f0c0dab35195082f47007e88556f31ef52935
eric mutua <erotush77@gmail.com>
2021-02-16 10:04:50 +0300

    added users-api/

---
+++ users-api/users-api.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/muesli/cache2go"
+)
+
+var cache = cache2go.Cache("data-store")
+
+type user interface{}
+
+type route struct {
+	pattern *regexp.Regexp
+	handler http.Handler
+}
+
+// RegexpHandler is not exported
+type RegexpHandler struct {
+	routes []*route
+}
+
+// Handler is not exported
+func (h *RegexpHandler) Handler(pattern *regexp.Regexp, handler http.Handler) {
+	h.routes = append(h.routes, &route{pattern, handler})
+}
+
+// HandleFunc is not exported
+func (h *RegexpHandler) HandleFunc(pattern *regexp.Regexp, handler func(http.ResponseWriter, *http.Request)) {
+	h.routes = append(h.routes, &route{pattern, http.HandlerFunc(handler)})
+}
+
+// ServeHTTP is not exported
+func (h *RegexpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	for _, route := range h.routes {
+		if route.pattern.MatchString(r.URL.Path) {
+			route.handler.ServeHTTP(w, r)
+			return
+		}
+	}
+	// no pattern matched; send 404 response
+	http.NotFound(w, r)
+}
+
+// GetPort is not exported
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
+}
+func listUsersHandler(w http.ResponseWriter, req *http.Request) {
+
+	if req.Method == http.MethodGet {
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Add("Host", "localhost:8080")
+
+		n := cache.Count()
+
+		payloads := make([]*cache2go.CacheItem, n)
+
+		for i := 0; i < n; i++ {
+			var r int
+			r = i + 1
+			payloads[i], _ = cache.Value(r)
+
+		}
+
+		allUsers := make([]user, n)
+
+		for user := 0; user < n; user++ {
+			allUsers[user] = payloads[user].Data()
+
+		}
+
+		var uID int
+		var i string
+		var b bool
+
+		if b = strings.Contains(req.RequestURI, ":"); b {
+			i = strings.Split(req.RequestURI, ":")[1]
+			uID, _ = strconv.Atoi(i)
+
+		} else {
+			w.WriteHeader(http.StatusOK)
+			str := fmt.Sprintf("%s", allUsers)
+			s := strings.ReplaceAll(str, "} {", "},{")
+			fmt.Fprintf(w, "%v", s)
+
+		}
+
+		if uID > 0 && uID <= n {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, "%+v", allUsers[int(uID)-1])
+		} else {
+			if b {
+				w.WriteHeader(http.StatusNotFound)
+				fmt.Fprintf(w, "{\"status\": 404,\"message\": \"resource not found\"}")
+			}
+
+		}
+	}
+}
+
+func createUsersHandler(w http.ResponseWriter, req *http.Request) {
+
+	buffer := make([]byte, req.ContentLength)
+
+	var userID int
+
+	if req.Method == http.MethodPost {
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Add("port", GetPort())
+
+		if _, err := req.Body.Read(buffer); err.Error() != "EOF" {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Println("[ERROR]: ", err)
+		}
+
+		userID += cache.Count()
+		userID++
+
+		stringBuffer := string(buffer)
+
+		if strings.Contains(stringBuffer, "name") && strings.Contains(stringBuffer, "gender") && strings.Contains(stringBuffer, "email") {
+			cache.Add(userID, 0, stringBuffer)
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, "{\"Success\": \"true\",\"Message\": \"User Created with id %d\",\"Content-Size\": %d}", userID, req.ContentLength)
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "{\"Success\": \"false\",\"Message\": \"Expected at least fields - name, gender and email\",\"Content-Size\": %d}", req.ContentLength)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+	}
+}
+
+func main() {
+
+	handler := &RegexpHandler{}
+
+	reg1, _ := regexp.Compile("/list-users:\\d")
+	handler.HandleFunc(reg1, listUsersHandler)
+
+	reg2, _ := regexp.Compile("/list-users")
+	handler.HandleFunc(reg2, listUsersHandler)
+
+	reg3, _ := regexp.Compile("/create-user")
+	handler.HandleFunc(reg3, createUsersHandler)
+
+	s := &http.Server{
+		Addr:           GetPort(),
+		Handler:        handler,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20, // 1 Mib
+	}
+
+	log.Println("[+] Server is listening on localhost:80")
+	log.Fatal(s.ListenAndServe())
+
+}
